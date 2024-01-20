1ba78bc851f4943866882924f6dfa7eb1b24c8cf
jimingkang <532893278@qq.com>
2024-01-20 14:11:20 -0600

    add iframe

---
+++ WebUI/src/app/video/video.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-video',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './video.component.html',
+  styleUrl: './video.component.scss'
+})
+export class VideoComponent {
+
+}
