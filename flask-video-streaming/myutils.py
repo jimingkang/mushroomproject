6f66ac36e6c548a08f61b2c5ca6b5dbd6551d961
jimingkang <532893278@qq.com>
2024-01-26 10:12:57 -0600

    backup servokit

---
+++ flask-video-streaming/myutils.py
@@ -0,0 +1,14 @@
+def getbroker():
+    broker = ''
+    redis_server = ''
+    try:
+        for line in open("../ip.txt"):
+            if line[0:6] == "broker":
+                broker = line[9:len(line) - 1]
+            if line[0:6] == "reddis":
+                redis_server = line[9:len(line) - 1]
+    except:
+        pass
+    print(broker)
+    print(redis_server)
+    return (broker,redis_server)
---
+++ flask-video-streaming/sms.py
@@ -0,0 +1,24 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+account_sid = "AC9ca9c49dfbf6c1c8ad5ed358bb112626"
+auth_token = "d49f502871325a5546cfc847377ac0bc"
+verify_sid = "VA07d46825a630477ffd3d416cfe13fe72"
+verified_number = "+14012126037"
+
+client = Client(account_sid, auth_token)
+
+verification = client.verify.v2.services(verify_sid) \
+  .verifications \
+  .create(to=verified_number, channel="sms")
+print(verification.status)
+
+otp_code = input("Please enter the OTP:")
+
+verification_check = client.verify.v2.services(verify_sid) \
+  .verification_checks \
+  .create(to=verified_number, code=otp_code)
+print(verification_check.status)
