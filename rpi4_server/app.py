cc3ba0520f5cb04d5405c0c746c86c40b170a23f
jimingkang <jimingkang@gmail.com>
2024-10-16 17:56:16 -0500

    add cv2.imshow for adjustment of gripper

--- rpi4_server/app.py
+++ rpi4_server/app.py
@@ -112,29 +112,30 @@ class MovePublisher(Node):
             if(label=="orange" or label=="carrot" ):
                 cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
                 cv2.putText(frame, label, (left, top - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.imshow("detect",frame)
 
 
         
         # Output the visual detection data, we will draw this on our camera preview window
-        annotated_frame = results[0].plot()
-        if annotated_frame != None:
+        #annotated_frame = results[0].plot()
+        if result.boxes != None:
             adjust_msg = String()
             adjust_msg.data = 'yes,%d,%d,%d' %(int(5),int(5),hi.z) 
             self._adjust_publisher.publish(adjust_msg)
 
             # Get inference time
-            inference_time = results[0].speed['inference']
-            fps = 1000 / inference_time  # Convert to milliseconds
-            text = f'FPS: {fps:.1f}'
+            #inference_time = results[0].speed['inference']
+            #fps = 1000 / inference_time  # Convert to milliseconds
+            #text = f'FPS: {fps:.1f}'
 
             # Define font and position
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            text_size = cv2.getTextSize(text, font, 1, 2)[0]
-            text_x = annotated_frame.shape[1] - text_size[0] - 10  # 10 pixels from the right
-            text_y = text_size[1] + 10  # 10 pixels from the top
+            #font = cv2.FONT_HERSHEY_SIMPLEX
+            #text_size = cv2.getTextSize(text, font, 1, 2)[0]
+            #text_x = annotated_frame.shape[1] - text_size[0] - 10  # 10 pixels from the right
+            #text_y = text_size[1] + 10  # 10 pixels from the top
 
             # Draw the text on the annotated frame
-            cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            #cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
         else:
             adjust_msg = String()
             adjust_msg.data = 'no,%d,%d,%d' %(int(5),int(5),hi.z) 
