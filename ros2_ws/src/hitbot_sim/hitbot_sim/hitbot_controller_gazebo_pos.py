cb3fc953c235437c515c298ccfc7cc2544a728c3
jimingkang <jimingkang@gmail.com>
2025-02-19 13:20:23 -0600

    add move action for ros2_ws/src/hitbot_sim/hitbot_sim/hitbot_controller_gazebo_pos.py

--- ros2_ws/src/hitbot_sim/hitbot_sim/hitbot_controller_gazebo_pos.py
+++ ros2_ws/src/hitbot_sim/hitbot_sim/hitbot_controller_gazebo_pos.py
@@ -9,9 +9,9 @@ from geometry_msgs.msg import PoseStamped,Pose
 import time
 
 from geometry_msgs.msg import PoseStamped
-from moveit_msgs.action import MoveGroup
+from moveit_msgs.action import MoveAction
 from rclpy.action import ActionClient
-
+from moveit_msgs.msg import MotionPlanRequest
 class HitbotControllerGazeboPos(Node):
     def __init__(self):
         super().__init__('hitbot_controller_gazebo_pos')
@@ -32,10 +32,10 @@ class HitbotControllerGazeboPos(Node):
         #self.group.set_max_acceleration_scaling_factor(0.5)
         #self.moveit=MoveItPy(node_name=self.get_name())
         #self.arm=self.moveit.get_plainning_component("scara_arm")
-        self._action_client = ActionClient(self, MoveGroup, '/move_group')
+        self._action_client = ActionClient(self, MoveAction, '/move_action')
 
     def send_goal(self, x, y, z):
-        goal_msg = MoveGroup.Goal()
+        goal_msg = MoveAction.Goal()
         goal_msg.request.workspace_parameters.header.frame_id = "base_link"
 
         # Define target pose
@@ -46,11 +46,36 @@ class HitbotControllerGazeboPos(Node):
         pose.pose.position.z = z
         pose.pose.orientation.w = 1.0  # Default orientation
 
-        goal_msg.request.goal_constraints.append(pose)
+        #goal_msg.request.goal_constraints.append(pose)
 
         self._action_client.wait_for_server()
         self._action_client.send_goal_async(goal_msg)
         self.get_logger().info(f"goal_msg:{goal_msg}")
+
+        motion_request = MotionPlanRequest()
+        motion_request.workspace_parameters.header.frame_id = "base_link"
+        motion_request.goal_constraints.append(pose)
+        goal_msg.request = motion_request
+
+        self._action_client.wait_for_server()
+        future = self._action_client.send_goal_async(goal_msg)
+
+        future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected by MoveIt2.')
+            return
+        
+        self.get_logger().info('Goal accepted, waiting for result...')
+        result_future = goal_handle.get_result_async()
+        result_future.add_done_callback(self.result_callback)
+
+    def result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Execution complete. Success: {result.error_code.val == 1}')
+
     def plan_and_show_trajectory(self):
         # Get the current position
         #start_pose = self.group.get_current_pose().pose
