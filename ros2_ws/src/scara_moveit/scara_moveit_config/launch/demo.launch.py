fffe802ed36cb4c1571808613353ff215abe593e
jimingkang <jimingkang@gmail.com>
2025-02-19 16:08:56 -0600

    add        .joint_limits(file_path=config/joint_limits.yaml)  # Joint limitsor motion planning

--- ros2_ws/src/scara_moveit/scara_moveit_config/launch/demo.launch.py
+++ ros2_ws/src/scara_moveit/scara_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("scara_description", package_name="scara_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("scara_description", package_name="scara_moveit_config").kinematics(file_path="config/kinematics.yaml")
     return generate_demo_launch(moveit_config)
