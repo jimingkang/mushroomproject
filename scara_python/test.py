d3421186fdb0fe50af7e6692eaf5712d733a87cf
jimingkang <jimingkang@gmail.com>
2025-02-25 13:07:47 -0600

    save test data t ocsv file

--- scara_python/test.py
+++ scara_python/test.py
@@ -1,57 +1,90 @@
+import threading
+import pandas as pd
+import time
+
+df = pd.DataFrame()
+
+    
+
 # This is a sample Python script.
 #import HitbotInterface as hi
 from HitbotInterface import HitbotInterface
-
-
+stop_event = threading.Event()
+hi = HitbotInterface(92);  # //92 is robotid? yes
+hi.net_port_initial()
+ret = hi.initial(1, 210);  # // I add you on wechat
+hi.is_connect()
+hi.is_collision()
+hi.unlock_position()
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+ 
+
 #import redis
+
+ 
 
 #redis_server='192.168.0.100'
 #pool = redis.ConnectionPool(host=redis_server, port=6379, decode_responses=True,password='jimmy')
 #r = redis.Redis(connection_pool=pool)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    hi=HitbotInterface(92); #//92 is robotid? yes
-    hi.net_port_initial()
-    ret=hi.initial(1,210); #// I add you on wechat
-    print(hi.is_connect())
-    print(hi.is_collision())
-    print(hi.stop_move())
-    print(hi.get_joint_state(2))
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(hi.unlock_position())
-    hi.get_scara_param()
-    print(hi.x,hi.y)
-    ret=hi.movel_xyz(300,0,0,-63,100)
-    #ret=hi.new_movej_xyz_lr(hi.x-100,hi.y,0,-63,20,0,1)
+ 
+
+def move():
+    ret=hi.movel_xyz(600,0,0,-63,100)
     hi.wait_stop()
-    print(ret)
-    #ret=hi.movel_xyz(hi.x-10,hi.y-110,-200,75,20)
-    #ret=hi.movej_angle(90,90,0,0,20,0)
-    #hi.wait_stop()
-    print(ret)
-    i=3
+    ret=hi.movel_xyz(300,0,0,0,100)
+    hi.wait_stop()
+    i=2
     while i<4:
         i=i+1
+        rett=hi.movel_xyz(hi.x-i*20,hi.y-i*20,-80,-63-i*20,100)
+        #hi.wait_stop()
+    stop_event.set()
+
+ 
+
+
+def printstatus():
+    pre = time.time()
+    cur = time.time()
+    global df
+    print("time-diff,x,y,z,r,angle1,angle2")
+    while not stop_event.is_set():
+        cur = time.time()
         hi.get_scara_param()
-        print(hi.x,hi.y,hi.z)
-        rett=hi.movel_xyz(hi.x-i*20,hi.y-i*20,0,-63,20)
-        print(rett)
-        hi.wait_stop()
-        hi.get_scara_param()
-        print(hi.x,hi.y)
-        hi.movel_xyz(hi.x+i*20,hi.y+i*20,0,-63,20)
-        hi.wait_stop()
+        data = {
+            "time":[cur-pre], 
+            "x":   [hi.x], 
+            "y":  [hi.y],
+            "z":   [hi.z], 
+            "r":   [hi.r*180/3.14],
+            "angle1":  [hi.angle1*180/3.14],  
+            "angle2":  [hi.angle2*180/3.14],     
 
+        }
+        df_new_rows = pd.DataFrame(data)
 
+        # Call Pandas.concat to create a new DataFrame from several existing ones
+        df = pd.concat([df, df_new_rows])
+        #hi.wait_stop()
+        #print("{},{},{},{},{},{}".format(cur-pre,)
+        time.sleep(0.02)
+        pre=cur
+    df.to_csv("data.csv",sep='\t', encoding='utf-8', index=False, header=True)
 
+ 
+
+ 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    watcher_thread = threading.Thread(target=move)
+    move_thread = threading.Thread(target=printstatus)
+    move_thread.start()
+    watcher_thread.start()
+    move_thread.join()
+    watcher_thread.join()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ 
